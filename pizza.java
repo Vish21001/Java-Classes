db66435d28c31dbfdbb74038b15ac17cda85b32f
Vish21001 <agarwalvishisht@gmail.com>
2025-12-06 10:48:01 -0500

    Implement Pizza class with properties and toString method
    Fix constructor parameter type for size and correct toString method syntax.

---
+++ pizza.java
@@ -0,0 +1,18 @@
+public class Pizza
+{
+    private String type;
+    private String toppings;
+    private String size;
+    
+    public Pizza(String theType, String theToppings, int theSize)
+    {
+        type = theType;
+        toppings = theToppings;
+        size = theSize;
+    }
+    
+    public String to String
+    {
+        return "The pizza's type is: " + type + "toppings are: " + toppings + "and size is: " + size;
+    }
+}
