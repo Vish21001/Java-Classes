9f009b755f694809a77b78d7ca0a6a86c660f0f6
Vish21001 <agarwalvishisht@gmail.com>
2025-12-11 20:37:32 -0500

    Create clothing.java

---
+++ clothing.java
@@ -0,0 +1,19 @@
+public class Clothing
+{
+    public String size;
+    public String color;
+    
+    public Clothing(String size, String color)
+    {
+        this.size = size;
+        this.color = color;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+}
